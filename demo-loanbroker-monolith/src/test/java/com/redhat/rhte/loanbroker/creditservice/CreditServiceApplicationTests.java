7396c2454ab47f1a253687c16a720bd73f68f96f
Marius Bogoevici <159684+mbogoevici@users.noreply.github.com>
2018-10-05 14:14:23 -0400

    Add missing test

---
+++ demo-loanbroker-monolith/src/test/java/com/redhat/rhte/loanbroker/creditservice/CreditServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.redhat.rhte.loanbroker.creditservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CreditServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
